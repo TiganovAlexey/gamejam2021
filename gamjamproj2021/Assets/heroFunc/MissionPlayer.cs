e945135bacbdce03a8c4df15953ae72a93cc1343
TiganovAlexey <atiganov@hotmail.com>
2021-11-06 21:28:19 +0300

    fix move Char and add first quest

---
+++ gamjamproj2021/Assets/heroFunc/MissionPlayer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissionPlayer : MonoBehaviour
+{
+
+    public bool quest; //  отображает название квеста на экране;
+    public string MissionText; // название квеста;
+    public string ObjectTag; // тэг объекта;
+    public bool MissionObjects; //отображает собран предмет или нет;
+    public int Money; // количество денег;
+
+
+    void OnGUI()
+    {
+
+        if (quest)
+        {
+            GUI.Label(new Rect(20, 80, 300, 30), " " + MissionText); // значение названия квеста будет браться из скрипта Misson Bot;
+            if (MissionObjects)
+            { // если предмет собран;
+                GUI.Label(new Rect(150, 80, 200, 30), "[Предмет собран]"); // выводит надпись;
+            }
+        }
+        GUI.Label(new Rect(20, 100, 100, 30), "Деньги: " + Money); // отображает количество денег на экране;
+    }
+
+        // Start is called before the first frame update
+        void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- gamjamproj2021/Assets/heroFunc/MoveHero.cs
+++ gamjamproj2021/Assets/heroFunc/MoveHero.cs
@@ -7,68 +7,23 @@ using UnityEngine;
 
 public class MoveHero : MonoBehaviour
 {
-    public float Speed = 0.3f;
 
-    //даем возможность выбрать тэг пола.
-    //так же убедитесь что ваш Player сам не относится к даному слою. 
-
-    //!!!!Нацепите на него нестандартный Layer, например Player!!!!
-    public LayerMask GroundLayer = 1; // 1 == "Default"
-
-    private Rigidbody _rb;
-    private CapsuleCollider _collider; // теперь прийдется использовать CapsuleCollider
-    //и удалите бокс коллайдер если он есть
-
-    private bool _isGrounded
-    {
-        get
-        {
-            var bottomCenterPoint = new Vector3(_collider.bounds.center.x, _collider.bounds.min.y, _collider.bounds.center.z);
-
-            //создаем невидимую физическую капсулу и проверяем не пересекает ли она обьект который относится к полу
-
-            //_collider.bounds.size.x / 2 * 0.9f -- эта странная конструкция берет радиус обьекта.
-            // был бы обязательно сферой -- брался бы радиус напрямую, а так пишем по-универсальнее
-
-            return Physics.CheckCapsule(_collider.bounds.center, bottomCenterPoint, _collider.bounds.size.x / 2 * 0.9f, GroundLayer);
-            // если можно будет прыгать в воздухе, то нужно будет изменить коэфициент 0.9 на меньший.
-        }
-    }
-
-    private Vector3 _movementVector
-    {
-        get
-        {
-            var horizontal = Input.GetAxis("Horizontal");
-            var vertical = Input.GetAxis("Vertical");
-
-            return new Vector3(horizontal, 0.0f, vertical);
-        }
-    }
+    public float movementSpeed = 10;
+    public float turningSpeed = 10;
+    public static float horizontal, vertical;
 
     void Start()
     {
-        _rb = GetComponent<Rigidbody>();
-        _collider = GetComponent<CapsuleCollider>();
-
-        //т.к. нам не нужно что бы персонаж мог падать сам по-себе без нашего на то указания.
-        //то нужно заблочить поворот по осях X и Z
-        _rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
-
-        //  Защита от дурака
-        if (GroundLayer == gameObject.layer)
-            Debug.LogError("Player SortingLayer must be different from Ground SourtingLayer!");
+       
     }
 
-    void FixedUpdate()
+    private void Update()
     {
-        MoveLogic();
+        horizontal = Input.GetAxis("Horizontal") * turningSpeed * Time.deltaTime;
+        transform.Rotate(0, horizontal, 0);
+        vertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+        transform.Translate(0,0, vertical);
     }
 
-    private void MoveLogic()
-    {
-        // т.к. мы сейчас решили использовать физическое движение снова,
-        // мы убрали и множитель Time.fixedDeltaTime
-        _rb.AddForce(_movementVector * Speed, ForceMode.Impulse);
-    }
+
 }
